4a67a28876bc5d79ab95e970d6a3d156b7a3268d
Tanya <tanyach1997@gmail.com>
2022-06-09 03:01:42 +0530

    Update convert_to_tflite.py

--- object_detection/post_training_quantization/convert_to_tflite.py
+++ object_detection/post_training_quantization/convert_to_tflite.py
@@ -1,1 +1,11 @@
+import tensorflow as tf
 
+
+def convert_tflite(tflite_model_path, save_model_path, optimization_method):
+
+  converter = tf.lite.TFLiteConverter.from_saved_model(save_model_path)
+  converter.optimizations = [optimization_method]
+  tflite_model = converter.convert()
+
+  with open(tflite_model_path, 'wb') as f:
+    f.write(tflite_model)
